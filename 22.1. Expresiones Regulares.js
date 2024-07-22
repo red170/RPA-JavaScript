2e8898c0503c443e1a960639ee817beca9b5c86d
Ernesto Portillo <82615639+red170@users.noreply.github.com>
2024-07-22 15:28:46 -0600

    Complementación de Contenidos
    - Expresiones Regulares

--- 22.1. Expresiones Regulares.js
+++ 22.1. Expresiones Regulares.js
@@ -1,5 +1,7 @@
 let text = document.getElementById('text').textContent
 
 let regEx = /lorem/gi
+let regEx2 = RegExp('lorem', 'gi')
+let regEx3 = RegExp('/lorem/', 'gi')
 
 console.log(regEx.test(text))
