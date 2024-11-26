d010be94d415247a49cce4d729440d85aed2ce0f
Ernesto Portillo <82615639+red170@users.noreply.github.com>
2024-11-25 20:57:10 -0600

    Actualizacion de Contenidos

--- 05. API's/33.Match Media.js
+++ 05. API's/33.Match Media.js
@@ -1,14 +1,26 @@
-console.log(innerWidth)
+/*
+    https://developer.mozilla.org/es/docs/Web/API/Window/matchMedia
 
-const mql = matchMedia('(min-width:400px)')
+    mql = window.matchMedia(mediaQueryString)
 
-const applyMatchMedia = mql =>{
+    mql viene de media query list, que es el objeto que se crea con el método matchMedia()
+    mediaQueryString es cualquier media query válida en CSS
+*/
+
+const title = document.getElementById('title')
+
+const mql = matchMedia('(min-width:400px) and (orientation: landscape)')
+
+const applyMatchMedia = mql => {
     mql.matches ?
+        //NO HACER ESTO NUNCA
         document.body.style.backgroundColor = 'red'
+        //ESTO SI
+        // title.classList.add('clase que sea')
         :
+        //NO HACER ESTO NUNCA
         document.body.style.backgroundColor = 'royalblue'
 }
 
-addEventListener('DOMContentLoaded', ()=> applyMatchMedia(mql)) 
-addEventListener('resize', ()=> applyMatchMedia(mql)) 
-applyMatchMedia(mql)
+addEventListener('resize', () => applyMatchMedia(mql))
+addEventListener('DOMContentLoaded', () => applyMatchMedia(mql))
