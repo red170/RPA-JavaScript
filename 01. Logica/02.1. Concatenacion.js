c4fb98ef06274009c2739bd4a3e9cff90a1d754e
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-06 20:21:51 -0600

    Carga de Contenido

---
+++ 01. Logica/02.1. Concatenacion.js
@@ -0,0 +1,13 @@
+cadena1 = 'Hola Ernesto '
+cadena2 = 'Como Estas'
+
+fraseResultado = cadena1 + cadena2
+
+console.log(fraseResultado)
+
+numero1 = 1
+numero2 = 7
+
+numeroResultado = `el primer numero es ${numero1} y el segundo es ${numero2}`
+
+console.log(numeroResultado)
