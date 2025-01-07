d9768de8cc7eb358d2c360ff1416af4c9c97f054
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-06 19:46:36 -0600

    Actualizacion de Archivos

--- 01. Logica/02. Operadores.js
+++ 01. Logica/02. Operadores.js
@@ -1,14 +1,29 @@
 // Operadores de Asignacion
 
-x = y 
-x += y
-x -= y
-x *= y
-x /= y
-x %= y
-x **= y
-x <<= y
-x >>= y
-x >>>= y
-x &= y
-x 
+x = y  //Asignacion -- (x=y)
+x += y //Asignacion de Adicion -- (x =x+y)
+x -= y //Asigancion de Sustraccion --(x=x-y)
+x *= y //Asignacion de Multiplicacion -- (x=x*y)
+x /= y //Asignacion de Division -- (x=x/y)
+x %= y //Asignacion de Resto -- (x=x%y)
+x **= y //Asignacion de Expònenciacion -- (x=x**y)
+x <<= y //Asigancion de Desplazamiento a la Izquierda -- (x=x<<y)
+x >>= y //Asigancion de Desplazamiento a la Derecha -- (x=x>>y)
+x >>>= y //Asigancion sin Signo de Desplazamiento a la Derecha -- (x=x>>>y)
+x &= y //Asignacion AND
+x ^= y //Asignacion XOR
+x |= y  //Asignacion OR
+
+// Operadores Aritmeticos
+
++ //Suma
+- //Resta
+-- //Decremento
+* //Multiplicacion
+/ //Division
+** //Exponenciacion
+++ //Incremento
+% //Modulo
+
+//Operadores
+
