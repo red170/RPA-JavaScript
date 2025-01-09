26f34bc3f812656730516d7f12224e60c7062843
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-08 21:48:49 -0600

    Actualizacion de Contenido

--- 01. Logica/05. Arreglos.js
+++ 01. Logica/05. Arreglos.js
@@ -1,7 +1,10 @@
-let arreglo = [] // Declaracion de Arreglo
+//Arreglos
+// Un arreglo es una estructura de datos que nos permite almacenar varios valores en una misma variable.
+
+let arreglo = [] // Declaracion de Arreglo haciendo de [] para contener los elementos
 
 arreglo = [1,2,3,4,5,6,7,8,9,0] // Llenado de Variable
 
-// Mostar elemento de un arreglo (como parametro se usa el Index del elemento)
-console.log(arreglo[7])
+console.log(arreglo) // Imprimir el arreglo
+console.log(arreglo[7]) //Imprimir un elemento del arreglo indicando la posicion dentro de []
 
