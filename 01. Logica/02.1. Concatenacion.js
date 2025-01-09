e5ad4591a038520f2534403fc29ff1706f5a77b9
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-08 20:39:23 -0600

    Actualizacion de Contenidos

--- 01. Logica/02.1. Concatenacion.js
+++ 01. Logica/02.1. Concatenacion.js
@@ -1,13 +1,25 @@
-cadena1 = 'Hola Ernesto '
-cadena2 = 'Como Estas'
+// Concatenacion de cadenas de texto
+    //Declaramos las variables
+    cadena1 = 'Hola Ernesto '
+    cadena2 = 'Como Estas'
 
-fraseResultado = cadena1 + cadena2
+    //Almacenamos el resultado de la concatenacion de las dos cadenas en una nueva variable usando el operador +
+    fraseResultado = cadena1 + cadena2 
 
-console.log(fraseResultado)
+    //Mostramos el resultado en consola
+    console.log(fraseResultado)
 
-numero1 = 1
-numero2 = 7
+// Concatenacion de numeros
+    //Declaramos las variables
+    numero1 = 1
+    numero2 = 7
 
-numeroResultado = `el primer numero es ${numero1} y el segundo es ${numero2}`
+    //Almacenamos el resultado de la concatenacion de los dos numeros en una nueva variable usando el operador + pero antes colocando un espacio entre ellos para indicar que no es una suma de valores sino una concatenacion de cadenas
+    numeroResultado = numero1 + ' ' + numero2
 
-console.log(numeroResultado)
+    //Mostramos el resultado en consola
+    console.log(numeroResultado)
+
+numeroResultado1 = `el primer numero es ${numero1} y el segundo es ${numero2}`
+
+console.log(numeroResultado1)
