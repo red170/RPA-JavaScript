cdcb78c1304f07a51e53de2d8dda26f519f62281
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-14 11:10:06 -0600

    Resolucion de Ejercicio

---
+++ 00. Pruebas & Ejercicios/01. Logica/Desciciones y Bucles.js
@@ -0,0 +1,38 @@
+// Ejercicio 1: Número Par o Impar
+// Escribe un programa que pida al usuario un número y determine si es par o impar utilizando una estructura if-else.
+let numero = 2
+
+if ((numero % 2) === 0)
+{
+  console.log('Es Par')
+}
+else
+{
+  console.log('Es Impar')
+}
+// Ejercicio 2: Calificación
+// Escribe un programa que pida al usuario una calificación (número entre 0 y 100) y muestre una letra de calificación (A, B, C, D, F) utilizando if-else if-else.
+
+// Ejercicio 3: Contador de Números
+// Escribe un programa que imprima los números del 1 al 100 utilizando un bucle for.
+
+// Ejercicio 4: Suma de Números
+// Escribe un programa que sume los números del 1 al 10 utilizando un bucle while y muestre el resultado.
+
+// Ejercicio 5: Tabla de Multiplicar
+// Escribe un programa que pida al usuario un número y muestre su tabla de multiplicar del 1 al 10 utilizando un bucle for.
+
+// Ejercicio 6: Factorial
+// Escribe un programa que pida al usuario un número y calcule su factorial utilizando un bucle for.
+
+// Ejercicio 7: Números Primos
+// Escribe un programa que imprima todos los números primos entre 1 y 100 utilizando un bucle for y una estructura if.
+
+// Ejercicio 8: Adivina el Número
+// Escribe un programa que genere un número aleatorio entre 1 y 100 y permita al usuario adivinarlo. El programa debe dar pistas si el número es mayor o menor hasta que el usuario lo adivine, utilizando un bucle while.
+
+// Ejercicio 9: Suma de Elementos de un Array
+// Escribe un programa que sume todos los elementos de un array de números utilizando un bucle for.
+
+// Ejercicio 10: Palíndromo
+// Escribe un programa que pida al usuario una palabra y determine si es un palíndromo (se lee igual de adelante hacia atrás) utilizando un bucle for y una estructura if.
