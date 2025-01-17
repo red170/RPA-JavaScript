16bbe1cdba431f9058793beb162daac65eb96f20
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-16 19:05:05 -0600

    Organizacion de Contenido

--- 00. Pruebas & Ejercicios/01. Logica/Funciones.js
+++ 00. Pruebas & Ejercicios/01. Logica/Funciones.js
@@ -1,55 +1,56 @@
 // Ejercicio 1: Simulador de cajero automático
+	// Objetivo: Crear una función que simule un cajero automático. La función debe recibir como parámetros el saldo actual de la cuenta, el monto a retirar y el tipo de cuenta (ahorros o corriente). La función debe verificar si hay suficiente saldo, si el monto a retirar es múltiplo de 10 y aplicar comisiones según el tipo de cuenta.
 
-// Objetivo: Crear una función que simule un cajero automático. La función debe recibir como parámetros el saldo actual de la cuenta, el monto a retirar y el tipo de cuenta (ahorros o corriente). La función debe verificar si hay suficiente saldo, si el monto a retirar es múltiplo de 10 y aplicar comisiones según el tipo de cuenta.
-// Consideraciones:
-// Puedes agregar más funcionalidades como: límite diario de retiros, tipos de comisiones diferentes, etc.
-// Investiga sobre las estructuras de control condicionales (if, else if, else) para tomar decisiones basadas en las condiciones del ejercicio.
+		function banco(montoRetiro) {
 
-function banco(montoRetiro) {
+			let fondoAhorros = 1500
+			let fondoCorriente = 500
+			let retiroAhorros
+			let retiroCorriente
+			let nuevoFondoAhorros
+			let nuevoFondoCorriente
 
-	let fondoAhorros = 1500
-	let fondoCorriente = 500
-	let retiroAhorros
-	let retiroCorriente
-	let nuevoFondoAhorros
-	let nuevoFondoCorriente
+			let cuenta = parseInt(prompt('Binevenido a Tu Banco ... Ingresa el Numero Correspondiente a la cuenta que deseas hacer el Retiro, 1. Cuenta de Ahorros 2. Cuenta Corriente'))
 
-	let cuenta = parseInt(prompt('Binevenido a Tu Banco ... Ingresa el Numero Correspondiente a la cuenta que deseas hacer el Retiro, 1. Cuenta de Ahorros 2. Cuenta Corriente'))
+			switch (cuenta) {
+				case 1: 
+				retiroAhorros = parseInt(prompt('Bienvenido a la Cuenta de Ahorros, Ingresa la Cantidad a Retirar'))
 
-	switch (cuenta) {
-		case 1: 
-		retiroAhorros = parseInt(prompt('Bienvenido a la Cuenta de Ahorros, Ingresa la Cantidad a Retirar'))
+					if (retiroAhorros < fondoAhorros ){
+						nuevoFondoAhorros = fondoAhorros - retiroAhorros
+						alert(`Tu retiro de Ejecuto de Forma Exitosa, Tienes un Fondo Restante de $ ${nuevoFondoAhorros}.00`)
+						break
+					}
+					else{
+						alert('FONDOS INSUFICIENTES')
+					}
+				case 2:
+					retiroCorriente = parseInt(prompt('Bienvenido a la Cuenta Corriente, Ingresa la Cantidad a Retirar'))
 
-			if (retiroAhorros < fondoAhorros ){
-				nuevoFondoAhorros = fondoAhorros - retiroAhorros
-				alert(`Tu retiro de Ejecuto de Forma Exitosa, Tienes un Fondo Restante de $ ${nuevoFondoAhorros}.00`)
-				break
+					if (retiroCorriente < fondoCorriente ){
+						nuevoFondoCorriente = fondoCorriente - retiroCorriente
+						alert(`Tu retiro de Ejecuto de Forma Exitosa, Tienes un Fondo Restante de $${nuevoFondoCorriente}.00`)
+						break
+					}
+					else{
+						alert('FONDOS INSUFICIENTES')
+					}
+				default:
+					alert('OPCION INVALIDA')
 			}
-			else{
-				alert('FONDOS INSUFICIENTES')
-			}
-		case 2:
-			retiroCorriente = parseInt(prompt('Bienvenido a la Cuenta Corriente, Ingresa la Cantidad a Retirar'))
+		}
 
-			if (retiroCorriente < fondoCorriente ){
-				nuevoFondoCorriente = fondoCorriente - retiroCorriente
-				alert(`Tu retiro de Ejecuto de Forma Exitosa, Tienes un Fondo Restante de $${nuevoFondoCorriente}.00`)
-				break
-			}
-			else{
-				alert('FONDOS INSUFICIENTES')
-			}
-		default:
-			alert('OPCION INVALIDA')
-	}
-}
+		let pin = parseInt(prompt('Ingresa el Pin para Continuar'))
 
-let pin = parseInt(prompt('Ingresa el Pin para Continuar'))
+		if (pin === 1707){
+			banco()
+		}
+		else{
+			alert('PIN ERRONEO')
+		}
 
-if (pin === 1707){
-	banco()
-}
-else{
-	alert('PIN ERRONEO')
-}
-170
+// Ejercicio 2: Generador de contraseñas seguras
+	// Objetivo: Crear una función que genere contraseñas seguras de una longitud determinada. La contraseña debe incluir al menos una mayúscula, una minúscula, un número y un carácter especial.
+
+// Ejercicio 3: Juego de adivinar el número
+	// Objetivo: Crear una función que simule un juego donde la computadora elige un número aleatorio entre 1 y 100 y el usuario debe adivinarlo. La función debe indicar si el número ingresado por el usuario es mayor, menor o igual al número secreto.
