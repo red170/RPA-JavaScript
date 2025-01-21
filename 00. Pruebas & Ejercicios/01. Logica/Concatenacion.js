157933f3f38dc64708b5dcbbf55f75a1cb73fa48
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-21 11:31:16 -0600

    Carga de Contenidos
    - Indicaciones de Ejercicios

---
+++ 00. Pruebas & Ejercicios/01. Logica/Concatenacion.js
@@ -0,0 +1,14 @@
+// 1. Crear un saludo personalizado:
+// Crea un programa que pida al usuario su nombre y luego muestre un mensaje de saludo personalizado concatenando su nombre con una frase.
+
+// 2. Formar una oración completa:
+// Crea tres variables: una para el sujeto, otra para el verbo y otra para el complemento. Concatena estas variables para formar una oración con sentido.
+
+// 3. Crear una dirección de correo electrónico:
+// Pide al usuario su nombre y apellido y crea una dirección de correo electrónico estándar concatenando ambas partes y agregando un dominio.
+
+// 4. Mostrar la fecha completa:
+// Utiliza las propiedades de la fecha (día, mes, año) y concaténalas para mostrar la fecha actual en un formato específico (por ejemplo, "Hoy es 25 de diciembre de 2023").
+
+// 5. Crear un enlace web dinámico:
+// Crea un enlace web concatenando la URL base con una variable que contenga el nombre de una página específica.
