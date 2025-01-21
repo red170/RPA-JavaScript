c8a2d181b1b9c5239f8dbc954257f4fe673b5f8b
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-21 11:37:25 -0600

    Carga de Contenido
    - Indicaciones de Ejercicios

---
+++ 00. Pruebas & Ejercicios/01. Logica/4. Objeto Math.js
@@ -0,0 +1,14 @@
+// 1. Generar un número aleatorio entre dos valores:
+// Crea una función que tome dos números como parámetros y devuelva un número aleatorio entero entre esos dos valores (incluyendo ambos).
+
+// 2. Redondear números:
+// Crea una función que tome un número decimal y lo redondee hacia arriba, hacia abajo o al número entero más cercano, según un parámetro adicional.
+
+// 3. Calcular la hipotenusa de un triángulo rectángulo:
+// Crea una función que calcule la hipotenusa de un triángulo rectángulo dados los catetos. Utiliza el teorema de Pitágoras y el método Math.sqrt().
+
+// 4. Generar un número aleatorio entre 0 y 1 con un número específico de decimales:
+// Crea una función que tome un número como parámetro y genere un número aleatorio entre 0 y 1 con ese número de decimales.
+
+// 5. Simular el lanzamiento de un dado:
+// Crea una función que simule el lanzamiento de un dado de seis caras y devuelva el número obtenido.
