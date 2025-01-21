290f66373e67783efc663e6d9eef3aedda5dc403
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-21 11:38:51 -0600

    Update 5. Ciclos de Descicion.js

---
+++ 00. Pruebas & Ejercicios/01. Logica/5. Ciclos de Descicion.js
@@ -0,0 +1,14 @@
+// 1. Calculadora básica con operaciones:
+// Crea un programa que pida al usuario dos números y una operación (+, -, *, /). Utiliza una estructura condicional (if o switch) para realizar la operación correspondiente y mostrar el resultado.
+
+// 2. Determinar si un número es par o impar:
+// Escribe un programa que pida al usuario un número y determine si es par o impar. Utiliza el operador módulo (%) para obtener el resto de la división entre 2.
+
+// 3. Calcular el descuento de una compra:
+// Crea un programa que calcule el precio final de una compra aplicando un descuento según el monto total. Utiliza una estructura condicional para determinar el porcentaje de descuento a aplicar. Por ejemplo, si el monto es mayor a $100, aplica un 10% de descuento.
+
+// 4. Simulador de un semáforo:
+// Crea un programa que simule un semáforo. Utiliza una estructura condicional para mostrar un mensaje diferente según el color del semáforo (rojo, amarillo, verde).
+
+// 5. Clasificar un número:
+// Escribe un programa que pida al usuario un número y determine si es positivo, negativo o cero. Utiliza una estructura condicional para mostrar el resultado correspondiente.
