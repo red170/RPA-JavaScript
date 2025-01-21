1c681281aa2937d913ca439ca9ba880bcc34f332
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-21 11:36:13 -0600

    Carga de Contenido
    - Indicaciones de Ejercicios

---
+++ 00. Pruebas & Ejercicios/01. Logica/3. Metodos String.js
@@ -0,0 +1,19 @@
+// Manipulación básica:
+
+    // Invertir una cadena: Crea una función que tome una cadena como entrada y devuelva la cadena invertida.
+
+    // Contar vocales: Escribe una función que cuente el número de vocales en una cadena dada.
+
+    // Convertir a mayúsculas y minúsculas: Crea un programa que pida al usuario una cadena y la convierta a mayúsculas y luego a minúsculas.
+    
+    // Reemplazar palabras: Escribe una función que reemplace todas las ocurrencias de una palabra en una cadena por otra.
+
+// Manipulación avanzada:
+
+    // Validar un correo electrónico: Crea una función que valide si una cadena de texto tiene el formato de un correo electrónico válido.
+
+    // Capitalizar la primera letra de cada palabra: Escribe una función que capitalice la primera letra de cada palabra en una cadena.
+
+    // Eliminar espacios en blanco: Crea una función que elimine todos los espacios en blanco al principio y al final de una cadena.
+
+    // Verificar si una cadena es un palíndromo: Escribe una función que determine si una cadena es un palíndromo (se lee igual de izquierda a derecha que de derecha a izquierda)
