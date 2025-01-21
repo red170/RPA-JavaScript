0a4bf78eb251cc72adf485bc15c1edfe985060d3
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-21 11:40:25 -0600

    Carga de Contenido
    -Indicaciones de Ejercicios

---
+++ 00. Pruebas & Ejercicios/01. Logica/6. Arrays.js
@@ -0,0 +1,15 @@
+// 1. Sumar todos los elementos de un array:
+
+// Crea una función que tome un array de números como argumento y devuelva la suma de todos sus elementos.
+// 2. Encontrar el número más grande en un array:
+
+// Escribe una función que encuentre el número más grande en un array de números.
+// 3. Invertir un array:
+
+// Crea una función que invierta el orden de los elementos de un array.
+// 4. Eliminar duplicados de un array:
+
+// Escribe una función que tome un array y devuelva un nuevo array con los mismos elementos pero sin duplicados.
+// 5. Ordenar un array de forma ascendente:
+
+// Crea una función que ordene los elementos de un array de números de menor a mayor.
