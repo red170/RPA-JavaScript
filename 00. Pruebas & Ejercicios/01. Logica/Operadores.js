d6b4885e8a07bf5ed05f7bc25e9da61b7943a282
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-21 11:29:36 -0600

    Carga de Contenido
    - Indicaciones de Ejercicios de Operadores

---
+++ 00. Pruebas & Ejercicios/01. Logica/Operadores.js
@@ -0,0 +1,14 @@
+// 1. Calculadora básica:
+// Crea un programa que pida al usuario dos números y una operación (+, -, *, /). Utiliza un switch para realizar la operación correspondiente y mostrar el resultado.
+
+// 2. Conversión de unidades:
+// Crea un programa que convierta de grados Celsius a Fahrenheit o viceversa. El usuario debe elegir la conversión y proporcionar el valor a convertir. Recuerda las fórmulas de conversión.
+
+// 3. Descuento en una compra:
+// Crea un programa que calcule el precio final de una compra aplicando un descuento. El usuario debe ingresar el precio original y el porcentaje de descuento.
+
+// 4. Área y perímetro de figuras:
+// Crea un programa que calcule el área y perímetro de diferentes figuras geométricas (círculo, rectángulo, triángulo). El usuario debe elegir la figura y proporcionar las medidas necesarias.
+
+// 5. Aumento de salario:
+// Crea un programa que calcule el nuevo salario de un empleado después de un aumento. El usuario debe ingresar el salario actual y el porcentaje de aumento.
