7e290b8fcd705ec4c5fac9b771e41a8662a0022a
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-27 15:55:30 -0600

    Carga de Contenido

---
+++ 01. Logica/06.1. Metodos (Sets).js
@@ -0,0 +1,14 @@
+//Intersection
+//Devuelve un nuevo conjunto que toma los elementos que se repiten entre otros dos conjuntos
+
+//Union
+//Devulve un Conjunto que Contiene todos los elementos de dos conjuntos dados sin repetir elementos
+
+//Diferencias
+//Devuleve un conjunto que contiene elementos de un conjunto que no tiene el otro conjunto
+
+//Diferencia Simetrica
+//Devuelve un conjunto com elementos que se encuentran en el primer conjunto o el o segundo pero no en ambos
+
+//Conjunto Hijo
+//Devulve un Valor Booleano que indidca si todos los elementos del conjunto primario estan en el segundo
