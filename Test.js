ded1d01b76246156e776b99194a8c5b6f65121ae
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-27 14:49:07 -0600

    Actualizacion de Contenido

--- Test.js
+++
@@ -1,9 +0,0 @@
-let array3 = [3,5,1,2,6,9,8,4,7]
-
-        function mayor(array1){
-            let ordenado = array1.sort(((a,b)=>a-b))       
-            console.log(ordenado)
-
-            
-        }
-        mayor(array3)
