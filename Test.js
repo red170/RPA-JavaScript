fef6644121aae15e64e3aff1da794a172e75899c
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-01-27 14:32:37 -0600

    Resolucion de Ejercicios

--- Test.js
+++ Test.js
@@ -1,10 +1,8 @@
-let array = [3,5,1,2,6,9,8,4,7]
+let numeros = [3, 5, 7, 2, 8, 1]
 
-function mayor(array1){
-    let ordenado = array1.sort(((a,b)=>b-a))       
-    console.log(ordenado[0])
-
-    
+function ordenar(array1){
+    let ordenado1 = array1.reverse()
+    console.log(ordenado1)
 }
 
-mayor(array)
+ordenar(numeros)
