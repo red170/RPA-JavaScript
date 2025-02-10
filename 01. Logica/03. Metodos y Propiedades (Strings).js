4b079489e10958b39f84d28c0a40ff7b5ae610ac
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-02-10 15:41:05 -0600

    Actualizacion de Contenido

--- 01. Logica/03. Metodos y Propiedades (Strings).js
+++ 01. Logica/03. Metodos y Propiedades (Strings).js
@@ -1,4 +1,4 @@
-let variable = ' soy un string '
+let variable = 'soy un string de ejemplo'
 
 //Propiedades
     // Las propiedades son valores que se pueden obtener de una variable
@@ -10,6 +10,21 @@ let variable = ' soy un string '
 
 // Metodos
     // Los metodos son funciones que se pueden aplicar a una variable
+
+    //Conactenar Strings
+        let concatenar = variable.concat('de ejemplo')
+        //Resultado
+        console.log('concatenar: ' + concatenar)
+
+    //Devuelve el caracter en la posicion que se le indique en el parametro
+        let caracter = variable.charAt(4)
+        //Resultado
+        console.log('caracterEnPosicion: ' + caracter)
+
+    // Convertir el Valor de una Variable a String
+        let aString = variable.toString()
+        //Resultado
+        console.log('Conversion a String' + aString)
 
     //Devuelva el string en Mayusculas
         let mayuscula = variable.toUpperCase() 
@@ -26,6 +41,11 @@ let variable = ' soy un string '
         //Resultado
         console.log('indexDeUnaCadena: ' + indice)
 
+    //Devuelve la ultima posicion del caracter o cadena de la variable quen se le indique en el parametro
+        let ultimoIndice = variable.lastIndexOf('O')
+        //Resultado
+        console.log('ultimoIndexDeUnaCadena: ' + ultimoIndice)
+
     //Reemplaza Caracteres (Busca el caracter o cadena ingresado en el primer parametro y lo reemplaza por el segundo parametro)
         let cambio = variable.replace('O','F')
         //Resultado
@@ -35,8 +55,18 @@ let variable = ' soy un string '
         let borrarEspacio = variable.trim() 
         //Resultado
         console.log('borrarEspaciosAlInicioYFinal: ' + borrarEspacio)
+    
+    //Elimina los espacios al inicio de una cadena
+        let borrarEspacioInicio = variable.trimStart()
+        //Resultado
+        console.log('borrarEspaciosAlInicio: ' + borrarEspacioInicio)
 
-    //Mediante los indices introducidos en los parametros se extrae una parte de la cadena
+    //Elimina los espacios al final de una cadena
+        let borrarEspacioFinal = variable.trimEnd()
+        //Resultado
+        console.log('borrarEspaciosAlFinal: ' + borrarEspacioFinal)
+
+    //Mediante los indices introducidos en los parametros se extrae una parte de la cadena, el prlmer parametro indica desde donde se empieza a extraer y el segundo hasta donde se extrae
         let subString = variable.substring('0','6') 
         //Resultado
         console.log('subCadena: ' + subString)
@@ -55,12 +85,28 @@ let variable = ' soy un string '
         let final = variable.endsWith('Y','4') 
         //Resultado
         console.log('finalizaCon: ' + final)
+
     //Devuleve True si la cadena incluye el caracter o cadena que se le indique en el parametro, el primer parametro es el caracter o cadena a buscar y el segundo parametro es el indice desde donde se empieza a buscar
         let include = variable.includes(' ','1')
         //Resultado
         console.log('incluye: ' + include)
 
+    //Rellena la cadena con el caracter que se le indique en el primer parametro hasta la longitud que se le indique en el segundo parametro
+        let padStart = variable.padStart(20,'*') 
+        //Resultado
+        console.log('rellenaInicio: ' + padStart)
+
+    //Igual que el anterior pero rellena al final
+        let padEnd = variable.padEnd(20,'*') 
+        //Resultado
+        console.log('rellenaFinal: ' + padEnd)
+
     // Repite la cadena las veces que indiquemos en el parametro
         let repeticion = variable.repeat(3) 
         //Resultado
         console.log('repiteCadena: ' + repeticion)
+
+    //Separa la cadena en un array de strings, el parametro indica el caracter que buscara para separar
+        let split = variable.split(' ')
+        //Resultado
+        console.log('separaCadena: ' + split)
