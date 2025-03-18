f6f62548f9ab05a281e4708eee04c5ad67225bf8
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-03-17 20:55:37 -0600

    Carga de Contenido

---
+++ 02. Consola/Funciones de Consola.js
@@ -0,0 +1,32 @@
+//Funciones de Registro en la consola
+    //Assert 
+    //Muestra un mensaje de error en la consola si la afirmación es falsa. Si la afirmación es verdadera, no se muestra nada.
+    console.assert(2>3, "5 no es mayor que 3");
+
+    //Clear
+    //Limpia la consola.
+    console.clear();
+
+    //Error
+    //Muestra un mensaje de error en la consola.
+    console.error("Esto es un error");
+
+    //Info
+    //Muestra un mensaje de información en la consola.
+    console.info("Esto es un mensaje de información");
+
+    //Log
+    //Muestra un mensaje en la consola.
+    console.log("Esto es un mensaje");
+
+    //Table
+    //Muestra datos en forma de tabla.
+    console.table(["Manzana", "Banana", "Naranja"]);
+
+    //Warn
+    //Muestra un mensaje de advertencia en la consola.
+    console.warn("Esto es un mensaje de advertencia");
+
+    //Dir
+    //Muestra una lista interactiva de las propiedades de un objeto JavaScript.
+    console.dir([1,2,3,4,5]);
