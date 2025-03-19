2875735bf0a6350e9bb07c94c9f344156403f02f
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-03-19 14:57:37 -0600

    Carga de Contenido

--- 03. Document Object Model/2.2. Edicion de Clases.js
+++ 03. Document Object Model/2.2. Edicion de Clases.js
@@ -2,17 +2,24 @@
 
 // Añadir una Clase
 
-title.classList.add('mainTitle')
+    title.classList.add('mainTitle')
 
 // Quitar una Clase
 
     title.classList.remove('mainTitle')
 
-// Devuelve boleano dependiendo si contiene lo que se consulta
-
-    if (title.classList.contains('title')) console.log('si esta')
-    else console.log('no esta')
-
 //Sustituir una clase por otra
 
     title.classList.replace('title','mainTitle')
+
+// Comprobar si un elemento tiene una clase
+
+    title.classList.contains('mainTitle')
+
+// Alternar una clase
+
+    title.classList.toggle('mainTitle')
+
+// Devuelve la Clase del Indice especificado
+
+    title.classList.item(0)
