8858c1581badee62d1315b54433fb2a916a91a33
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-03-18 20:10:07 -0600

    Carga de Contenido

---
+++ 03. Document Object Model/0.Definiciones.js
@@ -0,0 +1,11 @@
+//DOM
+//El DOM es una interfaz de programación de aplicaciones (API) para documentos HTML y XML. Define la estructura lógica de los documentos y la forma en que se accede y manipula un documento. En el caso de HTML, el DOM representa la estructura lógica de un documento y la forma en que el documento es leído y visualizado por el navegador.
+
+    //Nodos del DOM
+    //Los nodos son los elementos que conforman el DOM, estos pueden ser de diferentes tipos:
+        //1. Document: Representa todo el documento HTML.
+        //2. Element: Representa un elemento HTML.
+        //3. Text: Representa el contenido en texto de un elemento.
+        //4. Attribute: Representa los atributos de un elemento.
+        //5. Comment: Representa un comentario.
+
