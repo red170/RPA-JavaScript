63306da8594d3c19f9c8fa86961bb8a8318558e6
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-05-06 11:39:39 -0600

    Revision de Contenido

--- 03. Document Object Model/1.2. Obtencion y Modificacion de Elementos.js
+++ 03. Document Object Model/1.2. Obtencion y Modificacion de Elementos.js
@@ -1,12 +1,22 @@
 //** Text Content
 
-    // Se hace uso de la propiedad textContent para cambiar el texto dentro de un elemento
+    // Se hace uso de la propiedad textContent para cambiar el texto dentro de un elemento, sin incluir etiquetas HTML, muestar solamente el texto contenido en el elemento
     let title = document.getElementById('title')
-    title.textContent = 'DOM Mod'
+    title.textContent = 'DOM Modificado 1'
+
+//**Inner Content */
+
+    // Se hace uso de la propiedad innerText para cambiar el texto dentro de un elemento, incluyendo etiquetas HTML, pero no se muestra el HTML que no es visible en el navegador
+    let title1 = document.getElementById('title1')
+    let result = title1.innerText
+    console.log(result)
+
+    title1.innerText = 'DOM Modificado 2'
 
 //** Inner HTML
 
-    // Se hace uso de la propiedad innerHTML para cambiar el texto dentro de un elemento
+    // Se hace uso de la propiedad innerHTML para cambiar el texto dentro de un elemento, muestra solamente el texto que esta espcificado como visible en el HTML, mostrando las etiquetas del elemento que contiene el texto
+
     let title2 = document.getElementById('title2')
     title2.innerHTML = 'DOM <span>Mod</span>'
 
