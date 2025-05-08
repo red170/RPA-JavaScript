65b02f15df47c23c5c59b70aa3db4befb4cc3362
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-05-08 14:49:57 -0600

    Revision de Contenido

--- 03. Document Object Model/1.4. Obtencion y Modificacion de Elementos Childs.js
+++ 03. Document Object Model/1.4. Obtencion y Modificacion de Elementos Childs.js
@@ -3,35 +3,36 @@ const contenedor = document.getElementById('container')
 //**FirstChild
 
     // sirve para obtener el primer hijo de un nodo
-        const primerHijo = contenedor.firstChild
-        console.log(primerHijo)
+        const primerHijo = contenedor.firstChild //Asignar el primer hijo a una variable
+        console.log(primerHijo) //Mostrar el primer hijo en la consola
 
 //**LastChild
 
     // sirve para obtener el último hijo de un nodo
-        const ultimoHijo = contenedor.lastChild
-        console.log(ultimoHijo)
-
+        const ultimoHijo = contenedor.lastChild //Asignar el último hijo a una variable
+        console.log(ultimoHijo) ///Mostrar el último hijo en la consola
 //**FirstElementChild
 
     // sirve para obtener el primer hijo de un nodo que sea un elemento
-        const primerHijoElemento = contenedor.firstElementChild
-        console.log(primerHijoElemento)
+        const primerHijoElemento = contenedor.firstElementChild //Asignar el primer hijo elemento a una variable
+        console.log(primerHijoElemento) //Mostrar el primer hijo elemento en la consola
 
 //**LastElementChild
 
     // sirve para obtener el último hijo de un nodo que sea un elemento
-        const ultimoHijoElemento = contenedor.lastElementChild
-        console.log(ultimoHijoElemento)
+        const ultimoHijoElemento = contenedor.lastElementChild //Asignar el último hijo elemento a una variable
+        console.log(ultimoHijoElemento) //Mostrar el último hijo elemento en la consola
 
 //**ChildNodes 
 
     // sirve para obtener todos los nodos hijos de un nodo ( es recorrible con un forEach)
-        const nodosHijos = contenedor.childNodes
-        console.log(nodosHijos)
+        const nodosHijos = contenedor.childNodes //Asignar todos los nodos hijos a una variable
+        console.log(nodosHijos) //Mostrar todos los nodos hijos en la consola
 
 //**Children
 
     // sirve para obtener todos los nodos hijos de un nodo que sean elementos (es recorrible con bucles ForIn o ForOf)
-        const nodosHijosElementos = contenedor.children
-        console.log(nodosHijosElementos)
+        const nodosHijosElementos = contenedor.children //Asignar todos los nodos hijos elementos a una variable
+        console.log(nodosHijosElementos) //Mostrar todos los nodos hijos elementos en la consola
+        console.log(nodosHijosElementos[0]) //Mostrar el primer nodo hijo elemento en la consola
+        console.log(nodosHijosElementos[1]) //Mostrar el segundo nodo hijo elemento en la consola
