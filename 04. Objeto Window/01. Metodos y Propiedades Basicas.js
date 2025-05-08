9a0e4f55f5ae324101335a81effff6930ca939e7
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-05-08 15:35:52 -0600

    Carga de Contenido

---
+++ 04. Objeto Window/01. Metodos y Propiedades Basicas.js
@@ -0,0 +1,7 @@
+// Open
+// esta funcion abre una nueva ventana en el navegador
+window.open('https://www.youtube.com/watch?v=xOinGb2MZSk')
+
+//Close
+// esta funcion cierra la ventana actual del navegador
+window.close()
