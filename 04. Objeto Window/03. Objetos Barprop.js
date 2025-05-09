f5998d4e9d4b02304a6d08367b012ca6bb923377
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-05-09 14:10:15 -0600

    Carga de Contenido

---
+++ 04. Objeto Window/03. Objetos Barprop.js
@@ -0,0 +1,23 @@
+//Locationbar
+// Representa la barra de direcciones del navegador
+console.log(window.locationbar) // devuelve un objeto LocationBar que representa la barra de direcciones del navegador
+
+// Menubar
+// Representa la barra de menus del navegador
+console.log(window.menubar) // devuelve un objeto MenuBar que representa la barra de menus del navegador
+
+// Personalbar
+// Representa la barra de favoritos del navegador
+console.log(window.personalbar) // devuelve un objeto PersonalBar que representa la barra de favoritos del navegador
+
+// Scrollbars
+// Representa las barras de desplazamiento del navegador
+console.log(window.scrollbars) // devuelve un objeto ScrollBar que representa las barras de desplazamiento del navegador
+
+// Statusbar
+// Representa la barra de estado del navegador
+console.log(window.statusbar) // devuelve un objeto StatusBar que representa la barra de estado del navegador
+
+// Toolbar
+// Representa la barra de herramientas del navegador
+console.log(window.toolbar) // devuelve un objeto ToolBar que representa la barra de herramientas del navegador
