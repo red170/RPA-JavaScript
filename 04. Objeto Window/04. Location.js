a4294573841623953c2081108cf9c60d25fa1e30
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-05-09 14:16:07 -0600

    Carga de Contenido

---
+++ 04. Objeto Window/04. Location.js
@@ -0,0 +1,24 @@
+//Location HREF
+// sirve para obtener la URL completa de la página actual
+document.write(window.location.href) // devuelve la URL completa de la página actual
+document.write("<br>")
+
+//Location HostName
+// sirve para obtener el nombre del host de la URL actual
+document.write(window.location.hostname) // devuelve el nombre del host de la URL actual
+document.write("<br>")
+
+//Location Pathname
+// sirve para obtener la ruta de la URL actual
+document.write(window.location.pathname) // devuelve la ruta de la URL actual
+document.write("<br>")
+
+//Location Protocol
+// sirve para obtener el protocolo de la URL actual (http, https, ftp, etc.)
+document.write(window.location.protocol) // devuelve el protocolo de la URL actual
+document.write("<br>")
+
+//Location Assign
+// sirve para redirigir a una nueva URL
+// window.location.assign("https://www.google.com") // redirige a la URL especificada
+// document.write("<br>")
