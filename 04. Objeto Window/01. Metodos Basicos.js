0a52f9777ea751eb4e31537d75cd3460d5f433ae
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-05-09 14:06:44 -0600

    Carga de Contenido

--- 04. Objeto Window/01. Metodos Basicos.js
+++ 04. Objeto Window/01. Metodos Basicos.js
@@ -19,15 +19,31 @@
 
 //Prompt	
 // esta funcion muestra un cuadro de dialogo que permite al usuario ingresar un valor
-let nombre = window.prompt('Cual es tu nombre?')
-console.log(nombre)
+// let nombre = window.prompt('Cual es tu nombre?')
+// console.log(nombre)
 
 //Confirm
 // esta funcion muestra un cuadro de dialogo que permite al usuario confirmar o cancelar una accion
-let respuesta = window.confirm('Estas seguro de que quieres salir?')
-console.log(respuesta)
+// let respuesta = window.confirm('Estas seguro de que quieres salir?')
+// console.log(respuesta)
 
 //Scroll
 // esta funcion desplaza la ventana actual del navegador a una posicion especifica
-window.scroll(0, 0) // desplaza la ventana a la posicion (0, 0)
-window.scrollTo(0, 0) // desplaza la ventana a la posicion (0, 0)
+// window.scroll(0, 0) // desplaza la ventana a la posicion (0, 0)
+// window.scrollTo(0, 0) // desplaza la ventana a la posicion (0, 0)
+
+//ResizeBy
+// esta funcion cambia el tamaño de la ventana actual del navegador
+// window.resizeBy(100, 100) // cambia el tamaño de la ventana actual del navegador en 100px en x y 100px en y
+
+//ResizeTo
+// esta funcion cambia el tamaño de la ventana actual del navegador a un tamaño especifico
+// window.resizeTo(800, 600) // cambia el tamaño de la ventana actual del navegador a 800px en x y 600px en y
+
+// MoveBy
+// esta funcion desplaza la ventana actual del navegador a una posicion especifica
+// window.moveBy(100, 100) // desplaza la ventana actual del navegador en 100px en x y 100px en y
+
+// MoveTo
+// esta funcion desplaza la ventana actual del navegador a una posicion especifica
+// window.moveTo(100, 100) // desplaza la ventana actual del navegador a la posicion (100, 100)
