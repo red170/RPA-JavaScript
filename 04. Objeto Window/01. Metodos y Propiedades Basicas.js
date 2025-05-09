4aab68b30a13da7f65913b5cd9daefcdbbf7df3f
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-05-09 13:32:08 -0600

    Carga de Contenido

--- 04. Objeto Window/01. Metodos y Propiedades Basicas.js
+++ 04. Objeto Window/01. Metodos y Propiedades Basicas.js
@@ -1,7 +1,29 @@
+//** Obejto Window
+// El objeto window es el objeto global en el navegador, representando la ventana del navegador y proporcionando acceso a las funciones y propiedades del navegador.
+
 // Open
 // esta funcion abre una nueva ventana en el navegador
-window.open('https://www.youtube.com/watch?v=xOinGb2MZSk')
+// window.open('https://www.youtube.com/watch?v=xOinGb2MZSk')
 
 //Close
 // esta funcion cierra la ventana actual del navegador
-window.close()
+// window.close()
+
+//Alert
+// esta funcion muestra una alerta en el navegador
+// window.alert('Hola Mundo!')
+
+//Print
+// esta funcion imprime el contenido de la ventana actual del navegador
+// window.print()
+
+//Prompt	
+// esta funcion muestra un cuadro de dialogo que permite al usuario ingresar un valor
+let nombre = window.prompt('Cual es tu nombre?')
+console.log(nombre)
+
+//Confirm
+// esta funcion muestra un cuadro de dialogo que permite al usuario confirmar o cancelar una accion
+let respuesta = window.confirm('Estas seguro de que quieres salir?')
+console.log(respuesta)
+
