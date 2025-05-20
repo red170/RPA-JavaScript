7d184a3a9fa34e4c011596efdd440fb54c303fdf
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-05-20 11:12:02 -0600

    Carga de Contenido

---
+++ 06. Flujo del Programa/01. Explicaciones.js
@@ -0,0 +1,17 @@
+//*Sentencias de Bloque
+    //Las sentencias de bloque son aquellas que agrupan una o más sentencias en un bloque delimitado por llaves. son utilizadas para crear bloques de código que pueden ser ejecutados juntos.
+    //Las sentencias de bloque son útiles para organizar el código y controlar el flujo de ejecución. Por ejemplo, se pueden usar en estructuras de control como if, for, while, etc.
+
+//*Sentencias de Control de Flujo
+    //Las sentencias de control de flujo son aquellas que permiten controlar el flujo de ejecución del programa. Estas sentencias permiten tomar decisiones, repetir acciones o saltar a diferentes partes del código.
+        //!Las sentencias de control de flujo más comunes son:
+            //if, else, switch, for, while, do while, break y continue.
+
+    //Las sentencias de control de flujo son útiles para crear programas más complejos y dinámicos. Por ejemplo, se pueden usar para validar datos de entrada, realizar cálculos, iterar sobre colecciones de datos, etc.
+
+//*Sentencias de Manejo de Excepciones
+    //Las sentencias de manejo de excepciones son aquellas que permiten manejar errores o excepciones que pueden ocurrir durante la ejecución del programa. Estas sentencias permiten capturar errores y tomar acciones adecuadas para manejarlos.
+        //!Las sentencias de manejo de excepciones más comunes son:
+            //try, catch, finally y throw.
+
+    //Las sentencias de manejo de excepciones son útiles para crear programas más robustos y confiables. Por ejemplo, se pueden usar para validar datos de entrada, manejar errores de red, manejar errores de base de datos, etc.
