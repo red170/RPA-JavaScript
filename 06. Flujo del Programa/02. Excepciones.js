5d3fca3072c9e85836b16d13948f5e3355b2d34f
Ernesto Portillo <ernestoportillo170@gmail.com>
2025-05-21 10:50:24 -0600

    Carga de Contenido

---
+++ 06. Flujo del Programa/02. Excepciones.js
@@ -0,0 +1,10 @@
+//Excepciones
+// son estructuras de control que permiten manejar errores o situaciones excepcionales que pueden ocurrir durante la ejecucion de un programa
+
+    //Excepciones ECMA Script
+        // son errores que ocurren durante la ejecucion de un programa y que pueden ser manejados por el programador
+        
+    //Excepciones DOMExcepction
+        // son errores que ocurren en el DOM y que pueden ser manejados por el programador
+    //Excepciones DOMError
+        // son errores que ocurren en el DOM y que no pueden ser manejados por el programador
